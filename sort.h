0dbacdb1af461163c4a1d2fe900470612d19aca5
JessicaJulieth <3497@holbertonschool.com>
2021-10-27 21:30:09 -0500

    Betty to the header

--- sort.h
+++ sort.h
@@ -1,5 +1,5 @@
 #ifndef Sort_h
-#define Sort_h 
+#define Sort_h
 /* Library for sorting */
 #include <stdio.h>
 #include <stdlib.h>
@@ -14,9 +14,9 @@
  */
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
 /* Prototypes of the list */
